40171bf2d5d86c5d5a212dd7861b2b39c87cd08a
Emily Roller <eroller2@siebl-0224-32.ews.illinois.edu>
2019-09-05 11:59:50 -0500

    finished makefile (hopefully)

--- mp_intro/cs225/HSLAPixel.cpp
+++ mp_intro/cs225/HSLAPixel.cpp
@@ -6,6 +6,28 @@
  */
 
 #include "HSLAPixel.h"
+#include <cmath>
+#include <iostream>
 
 namespace cs225 {
+  HSLAPixel::HSLAPixel() {
+	h = 0;
+	s = 0;
+	l = 1.0;
+	a = 1.0;
+  }
+
+  HSLAPixel::HSLAPixel(double hue, double saturation, double luminance) {
+	h = hue;
+	s = saturation;
+	l = luminance;
+	a = 1;
+  }
+
+  HSLAPixel::HSLAPixel(double hue, double saturation, double luminance, double alpha) {
+	h = hue;
+	s = saturation;
+	l = luminance;
+	a = alpha;
+  }
 }
