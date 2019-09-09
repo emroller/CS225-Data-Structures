9061427929076a806b87b63c42034976776a58fc
Emily Roller <eroller2@siebl-0224-32.ews.illinois.edu>
2019-09-09 12:41:51 -0500

    myArt produces bright stripe pattern

--- mp_intro/intro.cpp
+++ mp_intro/intro.cpp
@@ -37,38 +37,43 @@ void rotate(std::string inputFile, std::string outputFile) {
 cs225::PNG myArt(unsigned int width, unsigned int height) {
   cs225::PNG png(width, height);
   // TODO: Part 3
+  // make entire background blue
   for (unsigned int x = 0; x < width; x++) {
     for (unsigned int y = 0; y < height; y++) {
-	HSLAPixel& pixel = png.getPixel(x,y);
-	if ( 0 < x <= width/5) {
-	  pixel.h = rand() % 361;
-	  pixel.s = rand() % 1;
-	  pixel.l = rand() % 1;
-	  pixel.a = rand() % 1;
-	} else if (width/5 < x <= (2 * width/5)) {
-	  pixel.h = rand() % 361;
-          pixel.s = rand() % 1;
-          pixel.l = rand() % 1;
-          pixel.a = rand() % 1;
-        } else if (2*width/5 < x <= 3 * width/5) {
-	  pixel.h = rand() % 361;
-          pixel.s = rand() % 1;
-          pixel.l = rand() % 1;
-          pixel.a = rand() % 1;
-        } else if (3*width/5 < x <= 4 * width/5) {
-	  pixel.h = rand() % 361;
-          pixel.s = rand() % 1;
-          pixel.l = rand() % 1;
-          pixel.a = rand() % 1;
-	} else {
-	  pixel.h = rand() % 361;
-          pixel.s = rand() % 1;
-          pixel.l = rand() % 1;
-          pixel.a = rand() % 1;
-	}
+	HSLAPixel& blue_pixel = png.getPixel(x,y);
+	  blue_pixel.h = 200;
+	  blue_pixel.s = 1;
+	  blue_pixel.l = .5;
+	  blue_pixel.a = 1;
     }
   }  
-
+  for (unsigned int x = 0; x < width; x += 4) {
+    for (unsigned int y = 0; y < height; y++) {
+	HSLAPixel& pixel = png.getPixel(x,y);
+	pixel.h = 90;
+	pixel.s = .5;
+	pixel.l = .8;
+	pixel.a = 1;
+    }
+  }
+  for (unsigned int x = 1; x < width; x +=4) {
+    for (unsigned int y = 0; y < height; y++) {
+	HSLAPixel& pixel = png.getPixel(x,y);
+	pixel.h = 330;
+	pixel.s = 1;
+	pixel.l = .8;
+	pixel.a = 1;
+    }
+  }
+  for (unsigned int x = 2; x < width; x+=4) {
+    for (unsigned int y = 0; y < height; y++) {
+	HSLAPixel& pixel = png.getPixel(x,y);
+	pixel.h = 0;
+	pixel.s = .9;
+	pixel.l = .7;
+	pixel.a = 1;
+    }
+  }
 return png;
 }
 
