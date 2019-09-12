dca584cd3f3a28c69fd9e155e9f832c30631fc42
Emily Roller <eroller2@siebl-0224-32.ews.illinois.edu>
2019-09-11 22:57:09 -0500

    added Image.cpp

---
+++ mp_stickers/Image.cpp
@@ -0,0 +1,51 @@
+ #include <iostream>
+using std::cerr;
+using std::endl;
+
+#include <string>
+using std::string;
+
+#include <vector>
+using std::vector;
+
+#include <cassert>
+#include <algorithm>
+#include <functional>
+
+#include "Image.h"  
+
+   void Image::darken(double amount) {
+   }
+
+   void Image::desaturate() {
+   }
+
+   void Image::desaturate(double amount) {
+   }
+
+   void Image::grayscale() {
+   }
+
+   void Image::illinify() {
+   }
+
+   void Image::lighten() {
+   }
+
+   void Image::lighten(double amount) {
+   }
+
+   void Image::rotateColor(double degrees) {
+   }
+
+   void Image::saturate() {
+   }
+
+   void Image::saturate(double amount) {
+   }
+
+   void Image::scale(double factor) {
+   }
+
+   void Image::scale(unsigned w, unsigned h) {
+   }
