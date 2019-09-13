e1d49f6bc102ed37fa4c90399fb7eea5249c8ccf
Emily Roller <eroller2@siebl-0224-32.ews.illinois.edu>
2019-09-13 12:19:26 -0500

    Finished darken, grayscale, and illinify functions

--- mp_stickers/Image.cpp
+++ mp_stickers/Image.cpp
@@ -15,11 +15,9 @@ using std::vector;
 #include "Image.h"  
 
    void Image::darken() {
-     unsigned w = this->width();
-     unsigned h = this->height();
      
-     for (unsigned x = 0; x < w; x++) {
-       for (unsigned y = 0; y < h; y++) {
+     for (unsigned x = 0; x < this->width(); x++) {
+       for (unsigned y = 0; y < this->height(); y++) {
 	 cs225:: HSLAPixel & pixel = this->getPixel(x,y);
 	 if (pixel.l < 0.1) {
 	   pixel.l = 0;
@@ -31,11 +29,9 @@ using std::vector;
    }
 
    void Image::darken(double amount) {
-     unsigned w = this->width();
-     unsigned h = this->height();
 
-     for (unsigned x = 0; x < w; x++) {
-       for (unsigned y = 0; y < h; y++) {
+     for (unsigned x = 0; x < this->width(); x++) {
+       for (unsigned y = 0; y < this->height(); y++) {
 	 cs225:: HSLAPixel & pixel = this->getPixel(x,y);
          if (pixel.l < amount) {
            pixel.l = 0;
@@ -47,15 +43,55 @@ using std::vector;
    }
 
    void Image::desaturate() {
+
+     for (unsigned x = 0; x < this->width(); x++) {
+       for (unsigned y = 0; y < this->height(); y++) {
+	 cs225:: HSLAPixel & pixel = this->getPixel(x,y);
+         if (pixel.s < 0.1) {
+           pixel.s = 0;
+         } else {
+           pixel.s -= 0.1;
+         }
+       }
+     }
    }
 
    void Image::desaturate(double amount) {
+
+     for (unsigned x = 0; x < this->width(); x++) {
+       for (unsigned y = 0; y < this->height(); y++) {
+	 cs225:: HSLAPixel & pixel = this->getPixel(x,y);
+         if (pixel.s < amount) {
+           pixel.s = 0;
+         } else {
+           pixel.s -= amount;
+         }
+       }
+     }
+
    }
 
    void Image::grayscale() {
+     for (unsigned x = 0; x < this->width(); x++) {
+       for (unsigned y = 0; y < this->height(); y++) {
+	 HSLAPixel & pixel = image.getPixel(x, y);
+	 pixel.s = 0;
+       }
+     }
    }
 
    void Image::illinify() {
+     for (unsigned int x = 0; x < this->width(); x++) {
+       for (unsigned int y = 0; y < this->height(); y++) {
+	 HSLAPixel & pixel = this->getPixel(x, y);
+	 // if the pixel hue is exactly in between 11 and 216, set it to orange
+	 if (pixel.h <= 113.5 || pixel.h >= 293.5) {
+	   pixel.h = 11.0;
+	 } else if (pixel.h > 113.5 && pixel.h < 293.5) {
+	   pixel.h = 216.0;
+	 }
+       }
+     } 
    }
 
    void Image::lighten() {
