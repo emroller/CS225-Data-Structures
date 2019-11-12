5d57eb2f2b64554bdfa47288dafc53cc90bd4843
Emily Roller <eroller2@siebl-0224-32.ews.illinois.edu>
2019-11-11 23:47:36 -0600

    skeleton of disjointsetsets class

--- mp_mazes/dsets.cpp
+++ mp_mazes/dsets.cpp
@@ -1,1 +1,19 @@
-/* Your code here! */
+#include "dsets.h"
+
+#include <iostream>
+using std::vector;
+
+void DisjointSets::addelements(int num) { }
+	
+int DisjointSets::find(int elem) {
+	return -1;
+}
+
+
+void DisjointSets::setunion(int a, int b) { }
+
+
+int DisjointSets::size(int elem) {
+	return -1;
+}
+
